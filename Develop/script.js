4d2d7cf8a55cb88caf32243d75f255f1d2bd1216
Rachel Cianfichi <cianfich@gmail.com>
2021-09-17 09:53:28 -0400

    Created password Array and updated pushing values from questions about what user wants to include.

--- Develop/script.js
+++ Develop/script.js
@@ -9,10 +9,11 @@ function writePassword() {
   passwordText.value = password;
   return password;
 }
-//var specialCharacters = [" ","!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\","]","^","_","`","{","|","}","~","""]
-var numbers = ["0","1","2","3","4","5","6","7","8","9"]
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var specialCharactersArray = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+var numbersArray = ["0","1","2","3","4","5","6","7","8","9"]
+var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var passwordArray = []
 
 
 function generatePassword(){
@@ -26,29 +27,30 @@ function generatePassword(){
         window.alert("Your password must be less than 128 characters long. Click OK to try again.")
     } else if (isNaN(characterLength)){
         window.alert("You must enter a numerical number. Click OK to try again.")
+      
     } else break; 
   }
 
   while ((specialCharacters !== true) && (numbers !== true) && (lowercase !== true) && (uppercase !== true)){
     var specialCharacters = window.confirm("Click OK to include special characters and Cancel to not include.")
     if (specialCharacters === true){
-      console.log("Great")
-    } else (console.log ("Not Great"))
+      passwordArray.push(specialCharactersArray)
+    } 
 
     var numbers = window.confirm("Click OK to include numbers and Cancel to not include.")
     if (numbers === true){
-      console.log("Great")
-    } else (console.log ("Not Great"))
+      passwordArray.push(numbersArray)
+    } 
 
     var lowercase = window.confirm("Click OK to include lowercase letters and Cancel to not include.")
     if (lowercase === true){
-      console.log("Great")
-    } else (console.log ("Not Great"))
+      passwordArray.push(lowercaseArray)
+    } 
 
     var uppercase = window.confirm("Click OK to include uppercase letters and Cancel to not include.")
     if (uppercase === true){
-      console.log("Great")
-    } else (console.log ("Not Great"))
+      passwordArray.push(uppercaseArray)
+    } 
 
     if ((specialCharacters !== true) && (numbers !== true) && (lowercase !== true) && (uppercase !== true)){
       window.alert("You must select at least one type of character to include in your password. Click OK to see selection options again.")
