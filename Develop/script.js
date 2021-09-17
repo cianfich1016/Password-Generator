fde5d5cd43805f111d4594b9816b9004b1d78e32
Rachel Cianfichi <cianfich@gmail.com>
2021-09-17 15:04:51 -0400

    Pushed the user chosen characters to the password arry by randomizing with randomization. Had to use the slice to eliminate characerLength mutlipled by 4. Chose to push characters from each array while password is created to ensure something of each selection was placed into password. This will make a new array with characterLength multiplied by however many selections the user hits OK to; therefore, slice allows to bring the password array back down to what the user requested ensuring characters they chose are there. Join brings the string of characters together.

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,7 @@ function writePassword() {
   passwordText.value = password;
   return password;
 }
-var specialCharactersArray = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+var specialCharactersArray = [' ','!','#','$','%','&',',','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@',"\\",']','^','_','`','{','|','}','~','"'];
 var numbersArray = ["0","1","2","3","4","5","6","7","8","9"]
 var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -28,38 +28,40 @@ function generatePassword(){
     } else if (isNaN(characterLength)){
         window.alert("You must enter a numerical number. Click OK to try again.")
       
-    } else break; 
+    }  
   }
 
   while ((specialCharacters !== true) && (numbers !== true) && (lowercase !== true) && (uppercase !== true)){
-    var specialCharacters = window.confirm("Click OK to include special characters and Cancel to not include.")
-    if (specialCharacters === true){
-      passwordArray.push(specialCharactersArray)
-    } 
-
-    var numbers = window.confirm("Click OK to include numbers and Cancel to not include.")
-    if (numbers === true){
-      passwordArray.push(numbersArray)
-    } 
-
-    var lowercase = window.confirm("Click OK to include lowercase letters and Cancel to not include.")
-    if (lowercase === true){
-      passwordArray.push(lowercaseArray)
-    } 
-
-    var uppercase = window.confirm("Click OK to include uppercase letters and Cancel to not include.")
-    if (uppercase === true){
-      passwordArray.push(uppercaseArray)
-    } 
-
+    var specialCharacters = window.confirm("Click OK to include special characters and Cancel to not include.");
+    
+    var numbers = window.confirm("Click OK to include numbers and Cancel to not include.");
+    
+    var lowercase = window.confirm("Click OK to include lowercase letters and Cancel to not include.");
+    
+    var uppercase = window.confirm("Click OK to include uppercase letters and Cancel to not include.");
+    
     if ((specialCharacters !== true) && (numbers !== true) && (lowercase !== true) && (uppercase !== true)){
       window.alert("You must select at least one type of character to include in your password. Click OK to see selection options again.")
     }
   }
 
+  characterLength = parseInt(characterLength);
 
-  
-  
+  for(var i=0; i < characterLength; i++){
+    if (specialCharacters === true){
+      passwordArray.push(specialCharactersArray[Math.floor(Math.random()*specialCharactersArray.length)])
+    }
+    if (numbers === true){
+      passwordArray.push(numbersArray[Math.floor(Math.random()*numbersArray.length)])
+    } 
+    if (lowercase === true){
+      passwordArray.push(lowercaseArray[Math.floor(Math.random()*lowercaseArray.length)])
+    } 
+    if (uppercase === true){
+      passwordArray.push(uppercaseArray[Math.floor(Math.random()*uppercaseArray.length)])
+    } 
+  }
+  return passwordArray.slice(0, characterLength).join("")
 } 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
