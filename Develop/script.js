43170d99d048a8bef01771783430c2ba169928db
Rachel Cianfichi <cianfich@gmail.com>
2021-09-17 15:48:49 -0400

    Added comments to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,10 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
   return password;
 }
+//Arrays to store various character selection options.
 var specialCharactersArray = [' ','!','#','$','%','&',',','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@',"\\",']','^','_','`','{','|','}','~','"'];
 var numbersArray = ["0","1","2","3","4","5","6","7","8","9"]
 var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
@@ -17,6 +17,7 @@ var passwordArray = []
 
 
 function generatePassword(){
+  //Prompt box asking for character length. Validating length and type with if statements. While loop runs through the prompts and corresponding alerts until a valid input is given.
   while ((characterLength === null) || (characterLength < 8) || (characterLength > 128) || (isNaN(characterLength))){
     var characterLength = window.prompt("How many characters would you like your password to contain?");
     if (characterLength === null){
@@ -31,6 +32,7 @@ function generatePassword(){
     }  
   }
 
+  //Confirm boxes for user to choose what kinds of characters to select. If user chooses none, an alert allows them to start over and make sure one is selected. While loop continues until at least one kind of character is selected.
   while ((specialCharacters !== true) && (numbers !== true) && (lowercase !== true) && (uppercase !== true)){
     var specialCharacters = window.confirm("Click OK to include special characters and Cancel to not include.");
     
@@ -44,9 +46,9 @@ function generatePassword(){
       window.alert("You must select at least one type of character to include in your password. Click OK to see selection options again.")
     }
   }
-
+  //Changes string type of user input to a number for length of password.
   characterLength = parseInt(characterLength);
-
+  //For loop adds selected characters to the password by randomizing selections from previously declared arrays. To ensure at least one of each type that is selected is generated in the password, each type that is selected is pushed to the array every loop.
   for(var i=0; i < characterLength; i++){
     if (specialCharacters === true){
       passwordArray.push(specialCharactersArray[Math.floor(Math.random()*specialCharactersArray.length)])
@@ -61,7 +63,10 @@ function generatePassword(){
       passwordArray.push(uppercaseArray[Math.floor(Math.random()*uppercaseArray.length)])
     } 
   }
+  //Slice method allows a new array to be created that is the proper length using the characterLength variable specified by the user but ensuring that at least 1 of every character he or she requested is there.
   return passwordArray.slice(0, characterLength).join("")
 } 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
